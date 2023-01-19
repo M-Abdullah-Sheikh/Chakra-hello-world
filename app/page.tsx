fc58461290e27bbf7ab43f9f3a18890abd497974
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2023-01-19 13:27:16 +0500

    assignment

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <Button colorScheme="green">Hello World</Button>
+    </div>
+  )
+}
